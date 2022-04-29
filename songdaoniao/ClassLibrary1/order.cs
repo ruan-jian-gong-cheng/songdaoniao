1f141d4ff755ecacf38b8bc4c1b82dbcccdc1a21
1zn2 <103159195+1zn2@users.noreply.github.com>
2022-04-29 20:38:51 +0800

    Create order.cs

---
+++ songdaoniao/ClassLibrary1/order.cs
@@ -0,0 +1,48 @@
+namespace ClassLibrary1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("order")]
+    public partial class order
+    {
+        [Key]
+        [StringLength(30)]
+        public string OrderNumber { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Text { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string ClientID { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string RunnerID { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string ClientPhone { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string RunnerPhone { get; set; }
+
+        [StringLength(100)]
+        public string Evaluate { get; set; }
+
+        [StringLength(10)]
+        public string Tip { get; set; }
+
+        public virtual client client { get; set; }
+
+        public virtual runner runner { get; set; }
+    }
+}
