f2e1d87dafe2fef23ac25e634acb210372ae1c49
1zn2 <103159195+1zn2@users.noreply.github.com>
2022-04-29 20:38:33 +0800

    Create client.cs

---
+++ songdaoniao/ClassLibrary1/client.cs
@@ -0,0 +1,30 @@
+namespace ClassLibrary1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("client")]
+    public partial class client
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public client()
+        {
+            order = new HashSet<order>();
+        }
+
+        [StringLength(30)]
+        public string ClientID { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string CardNumber { get; set; }
+
+        public virtual account account { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<order> order { get; set; }
+    }
+}
