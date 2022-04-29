5a61f0b866a77a1bf6a2eaee24822ac5825bf1cc
1zn2 <103159195+1zn2@users.noreply.github.com>
2022-04-29 20:37:26 +0800

    Create account.cs

---
+++ songdaoniao/ClassLibrary1/account.cs
@@ -0,0 +1,56 @@
+namespace ClassLibrary1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("account")]
+    public partial class account
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public account()
+        {
+            client = new HashSet<client>();
+            EvaluationForm = new HashSet<EvaluationForm>();
+            runner = new HashSet<runner>();
+        }
+
+        [Key]
+        [StringLength(30)]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Nickname { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Telephone { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string Address { get; set; }
+
+        [StringLength(30)]
+        public string Email { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<client> client { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<EvaluationForm> EvaluationForm { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<runner> runner { get; set; }
+    }
+}
