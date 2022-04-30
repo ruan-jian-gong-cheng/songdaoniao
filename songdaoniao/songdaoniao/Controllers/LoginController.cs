41615cea84dad95d72bde3bd62ac9cbdc9252184
1zn2 <103159195+1zn2@users.noreply.github.com>
2022-04-30 21:39:26 +0800

    Update LoginController.cs

--- songdaoniao/songdaoniao/Controllers/LoginController.cs
+++ songdaoniao/songdaoniao/Controllers/LoginController.cs
@@ -17,6 +17,26 @@ namespace songdaoniao.Controllers
             return View();
         }
 
+        public ActionResult Ceshi()
+        {
+            return View();
+        }
+
+        public ActionResult Paotui()
+        {
+            return View();
+        }
+
+        public ActionResult Ceshi1()
+        {
+            return View();
+        }
+
+        public ActionResult Ceshi2()
+        {
+            return View();
+        }
+
         public ActionResult Login()
         {
             account account = new account();
@@ -30,9 +50,19 @@ namespace songdaoniao.Controllers
             }
 
             var data = model1.account.Where(p => p.CardNumber == CardNumber && p.Password == Password).FirstOrDefault();
+            var a = model1.runner.Where(p => p.CardNumber == CardNumber).FirstOrDefault();
+
             if (data != null)
             {
-                return Content("<script>alert('登陆成功');location.href='Index'</script>");
+                if (a == null)
+                {
+                    return Content("<script>alert('登陆成功');location.href='ceshi'</script>");
+                }
+                else
+                {
+                    return Content("<script>alert('登陆成功');location.href='paotui'</script>");
+                }
+                
             }
             else
             {
@@ -40,6 +70,18 @@ namespace songdaoniao.Controllers
             }
 
 
+        }
+
+        public ActionResult Reque()
+        {
+            Model1 model1 = new Model1();
+            runner runner = new runner();
+            runner.CardNumber= Request.Form["CardNumber"];
+            runner.RunnerID = (Convert.ToInt32((model1.runner.OrderByDescending(d => d.RunnerID).FirstOrDefault()).RunnerID) + 1).ToString();
+            
+            model1.runner.Add(runner);
+            model1.SaveChanges();
+            return Content("<script>alert('申请成功！');location.href='paotui'</script>");
         }
     }
 }
